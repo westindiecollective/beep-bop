a9df532f49d88340359600ed0db1f61594b8d4f6
Christopher Humphries <engleek@gmail.com>
2017-01-21 13:51:25 +0100

    Added server

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const { createStore, applyMiddleware } = require('redux');
+const { handleActions } = require('redux-actions');
+const createSagaMiddleware = require('redux-saga').default;
+
+const actions = require('../src/actions');
+const sagas = require('./sagas');
+const createSocketMiddleware = require('./sockets');
+
+const defaultState = {
+  host: undefined,
+};
+
+const reducer = handleActions({
+  HELLO_SERVER: (state, action) => ({
+      host: uuid,
+  }),
+}, defaultState);
+
+const sagaMiddleware = createSagaMiddleware();
+const socketMiddleware = createSocketMiddleware({ port: 4000 });
+
+const store = createStore(
+  reducer,
+  applyMiddleware(sagaMiddleware, socketMiddleware)
+);
+
+sagaMiddleware.run(sagas)
