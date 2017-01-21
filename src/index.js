535e9216805c55b489824c21c9409ba466984148
Christopher Humphries <engleek@gmail.com>
2017-01-21 13:51:52 +0100

    Added client index with middleware and sockets

--- src/index.js
+++ src/index.js
@@ -1,7 +1,39 @@
+require('babel-runtime');
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { createStore, applyMiddleware } from 'redux'
+import { handleActions } from 'redux-actions';
+const createSagaMiddleware = require('redux-saga').default;
+
+const sagas = require('./sagas');
+const createSocketMiddleware = require('./sockets');
+
 import './index.css';
+
+import App from './App';
+
+const actions = require('./actions');
+
+const defaultState = {
+  uuid: undefined,
+};
+
+const reducer = handleActions({
+  HELLO_CLIENT: (state, { uuid }) => ({
+    uuid,
+  }),
+}, defaultState);
+
+const sagaMiddleware = createSagaMiddleware();
+const socketMiddleware = createSocketMiddleware({ port: 4000 });
+
+const store = createStore(
+  reducer,
+  applyMiddleware(sagaMiddleware, socketMiddleware)
+);
+
+sagaMiddleware.run(sagas);
 
 ReactDOM.render(
   <App />,
