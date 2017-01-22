3cbe6a946a73d40d2b9e690c9b36e54f18379558
Francois EOCHE <francoiseoche@gmail.com>
2017-01-22 10:21:55 +0100

    ARE YOU READYYYYYYYYYYYYYY

--- src/Host/Game.js
+++ src/Host/Game.js
@@ -1,10 +1,35 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class Game extends Component {
+  constructor() {
+    super();
+    this.state = {
+      timer: 90
+    }
+    this.tick = this.tick.bind(this);
+  }
+
+  tick() {
+    this.setState({ timer: this.state.timer && (this.state.timer - 1) });
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.tick(), 1000);
+  }
+
   render() {
     return (
       <div className="Game">
-        <p>What are you waiting for? Play on your device!</p>
+        <div className="Ingame-container">
+          <p>Hey, what are you waiting for?</p>
+          {/*{state === 'PLAYING.WARMUP' && <p></p>}*/}
+          {/* if PLAYING.ANSWERING state */}
+          {this.state.timer > 70 ? <div>Feel free to try out your sounds for this round on your device!<br/><div className="timer">{this.state.timer}</div></div> : this.state.timer? (<div><p>Now record your sounds on your device!</p><div className="timer recording">{this.state.timer}</div></div>) : (<p>Time's up !</p>)}
+        </div>
+        {/* if PLAYING.VOTING state */}
+        <div className="Votes-container">
+          {/* for each situation, repeat that */}
+        </div>
       </div>
     );
   }
--- src/Player/Game.js
+++ src/Player/Game.js
@@ -4,12 +4,11 @@ class Game extends Component {
   render() {
     return (
       <div className="Game">
-        <p>What are you waiting for? Play!</p>
-
         <div className="Situation">
           Test
           <span className="Dialog-bg"></span>
         </div>
+
       </div>
     );
   }
