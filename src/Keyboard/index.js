360a562d6c05cad11c443a3428ee29d4814fcc9c
Christopher Humphries <engleek@gmail.com>
2017-01-22 06:40:33 +0100

    Keyboard can record.

--- src/Keyboard/index.js
+++ src/Keyboard/index.js
@@ -30,27 +30,74 @@ const propTypes = {
 class Keyboard extends Component {
   constructor(props) {
     super(props);
+
     this.sampler = new Tone.Sampler({
       url: props.url,
       retrigger: true,
     }).toMaster();
+
+    this.playNote = this.playSample.bind(this);
+    this.record = this.record.bind(this);
+    this.play = this.play.bind(this);
+
+    this.events = [];
+
+    this.state = {
+      recording: false,
+    };
+  }
+
+  playSample(pitch) {
+    this.sampler.triggerAttackRelease(pitch, '8n');
+
+    if (this.state.recording) {
+      const time = Tone.Time(Tone.Transport.seconds);
+      this.events.push({ time: time.toNotation(), pitch });
+    }
+  }
+
+  record() {
+    if (this.state.recording) {
+      Tone.Transport.stop();
+      this.setState({ recording: false });
+      console.log(JSON.stringify(this.events));
+    } else {
+      Tone.Transport.start();
+      this.setState({ recording: true });
+    }
+  }
+
+  play() {
+    if (!this.state.recording) {
+      const part = new Tone.Part((time, event) => {
+        this.sampler.triggerAttackRelease(event.pitch, '8n', time);
+      }, this.events);
+      part.start(0);
+      Tone.Transport.start();
+    }
   }
 
   render() {
     return (
-      <div className="keys">
-        {whitekeys.map(({ note, pitch }) => (
-          <button
-            className={`key white ${note[0]}`}
-            onMouseDown={() => this.sampler.triggerAttackRelease(pitch, '8n')}
-          />
-        ))}
-        {blackkeys.map(({ note, pitch }) => (
-          <button
-            className={`key black ${note[0]}`}
-            onMouseDown={() => this.sampler.triggerAttackRelease(pitch, '8n')}
-          />
-        ))}
+      <div>
+        <button onClick={this.record}>{this.state.recording ? 'stop' : 'record'}</button>
+        <button onClick={this.play} disabled={this.state.recording}>play</button>
+        <div className="keys">
+          {whitekeys.map(({ note, pitch }) => (
+            <button
+              key={note}
+              className={`key white ${note[0]}`}
+              onMouseDown={() => this.playSample(pitch)}
+            />
+          ))}
+          {blackkeys.map(({ note, pitch }) => (
+            <button
+              key={note}
+              className={`key black ${note[0]}`}
+              onMouseDown={() => this.playSample(pitch)}
+            />
+          ))}
+        </div>
       </div>
     );
   }
