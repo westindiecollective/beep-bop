3fafd597ce055bd44d0c2ddf63cb3c2ce3386519
Christopher Humphries <engleek@gmail.com>
2017-01-22 10:01:57 +0100

    Added start of round logic and actions

--- server/sagas.js
+++ server/sagas.js
@@ -1,5 +1,24 @@
-const { put, takeEvery, takeLatest, select } = require('redux-saga/effects');
+const { put, take, takeEvery, takeLatest, select } = require('redux-saga/effects');
 const { addPlayer, addedToLobby, syncPlayers, gameStarted } = require('../src/actions');
+
+// Fisher-Yates shuffle
+function shuffle(array) {
+  let arrayCopy = array.slice();
+  let currentIndex = array.length, temporaryValue, randomIndex;
+
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    temporaryValue = arrayCopy[currentIndex];
+    arrayCopy[currentIndex] = arrayCopy[randomIndex];
+    arrayCopy[randomIndex] = temporaryValue;
+  }
+
+  return arrayCopy;
+}
+
+const sentences = require('../src/sentences.json');
 
 function* enterLobby({ payload, meta }) {
   const players = yield select(state => state.players);
@@ -27,6 +46,29 @@ function* startGame() {
   const players = yield select(state => Object.keys(state.players));
 
   yield [].concat([host], players).map((key) => put(gameStarted(key)));
+
+  const randomizedSentences = shuffle(sentences).slice(0, Object.keys(players).length - 1);
+  const round = Object.keys(players).map((id, index) => ({
+    id,
+    firstSentence: randomizedSentences[index % sentences.length],
+    firstAnswer: null,
+    secondSentence: randomizedSentences[(index + 1) % sentences.length],
+    secondAnswer: null,
+  }));
+
+  yield round.forEach(({ id, firstSentence }) => put(firstSentence(id, firstSentence )));
+
+  while (round.filter(player => player.firstAnswer === null).length > 0) {
+    const { id, answer } =  take('FIRST_ANSWER');
+    round.find(player => player.id === id).firstAnswer = answer;
+  }
+
+  yield round.forEach(({ id, secondSentence }) => put(secondSentence(id, secondSentence )));
+
+  while (round.filter(player => player.secondAnswer === null).length > 0) {
+    const { id, answer } =  take('SECOND_ANSWER');
+    round.find(player => player.id === id).secondAnswer = answer;
+  }
 }
 
 function* watchActions() {
--- src/actions.js
+++ src/actions.js
@@ -4,12 +4,16 @@ module.exports = {
   // CLIENT TO SERVER
   enterLobby: createAction('ENTER_LOBBY', (id, username) => ({ id, username, outgoing: true })),
   startGame: createAction('START_GAME', (id) => ({ id, outgoing: true })),
+  firstAnswer: createAction('FIRST_ANSWER', (id, answer) => ({ id, answer })),
+  secondAnswer: createAction('SECOND_ANSWER', (id, answer) => ({ id, answer })),
 
   // SERVER TO CLIENT
   setHost: createAction('SET_HOST', (dst) => ({ dst })),
   addedToLobby: createAction('ADDED_TO_LOBBY', (dst, role) => ({ dst, role })),
   syncPlayers: createAction('SYNC_PLAYERS', (dst, players) => ({ dst, players })),
   gameStarted: createAction('GAME_STARTED', (dst) => ({ dst })),
+  firstSentence: createAction('FIRST_QUESTION', (dst, sentence) => ({ dst, sentence })),
+  secondSentence: createAction('SECOND_QUESTION', (dst, sentence) => ({ dst, sentence })),
 
   // CLIENT
 
